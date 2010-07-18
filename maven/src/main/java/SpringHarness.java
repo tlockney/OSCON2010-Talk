b14885acf9cf9f719412b4da1da3865f21264428
Trenton Lipscomb <trentonl@gmail.com>
2010-07-18 13:31:52 -0700

    initial revision

---
+++ maven/src/main/java/SpringHarness.java
@@ -0,0 +1,25 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ */
+public class SpringHarness {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("app.xml");
+
+        // check value of the singleton
+        String singleton = (String) ctx.getBean("single");
+        assertThat(singleton, is("the singleton"));
+
+        // check that singletons are singletones and prototoypes, prototypes
+        assertTrue(ctx.getBean("single") == ctx.getBean("single"));
+    }
+}
